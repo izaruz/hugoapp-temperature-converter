023255103a3059211a621d866041a65167736485
Isaac <izaruz@gmail.com>
2020-12-03 22:50:31 -0600

    Nuevo componente para convertir temperaturas C y F

--- README.md
+++ README.md
@@ -1,24 +1,12 @@
-# temperature-converter
+# counter
 
-## Project setup
-```
-yarn install
-```
+Counter es un ejercicio realizado con la finalidad de tener un contador, el boton "COUNT" incrementa una variable de uno en uno y la muestra en pantalla.
 
-### Compiles and hot-reloads for development
-```
-yarn serve
-```
+## Pasos para ejecutar el ejercicio
 
-### Compiles and minifies for production
-```
-yarn build
-```
+1. Clonar el repositorio
+2. Ejecutar **yarn install** para instalar las dependencias de node necesarias para correr el app de Vue
+3. Ejecutar el comando **yarn serve** este comando se encargara de levantar un servidor virtual donde corre el ejercicio
+4. En un navegador ingresar a la ruta **localhost** que se muestra cuando termine de ejecutar el comando **yarn serve**
 
-### Lints and fixes files
-```
-yarn lint
-```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+**_Nota: Se utilizo Vue para el ejercicio porque actualmente estoy desarrollando aplicaciones en esta tecnología y se me hace comodo su uso_**.
---
+++ src/components/Converter.vue
@@ -0,0 +1,84 @@
+<template>
+    <section class="converter">
+        <div class="row justify-content-center">
+            <div class="col-12">
+                <h3>Converter</h3>
+            </div>
+            <div class="col-6">
+                <div class="row">
+                    <div class="form-group col-6">
+                        <label for="celsius" class="col-12 col-form-label text-left">Celsius</label>
+                        <input
+                            id="celsius"
+                            type="number"
+                            class="form-control"
+                            name="celsius"
+                            value
+                            required
+                            autofocus
+                            v-model="celsius"
+                            v-on:input="convert($event, 1)"
+                        />
+                    </div>
+                    <div class="form-group col-6">
+                        <label for="fahrenheit" class="col-12 col-form-label text-left">Fahrenheit</label>
+                        <input
+                            id="fahrenheit"
+                            type="number"
+                            class="form-control"
+                            name="fahrenheit"
+                            value
+                            required
+                            autofocus
+                            v-model="fahrenheit"
+                            v-on:input="convert($event, 2)"
+                        />
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+    </section>
+</template>
+
+<script>
+export default {
+    name: "Converter",
+    data() {
+        return {
+            temperature: 0,
+            celsius: 0,
+            fahrenheit: 0
+        }
+    },
+    methods: {
+        addCounter() {
+            this.counter++
+        },
+        convert(ev, type) {
+            if (type === 1) {
+                if (!isNaN(this.celsius)) {
+                    this.fahrenheit = this.celsius * (9/5) + 32
+                    this.fahrenheit = this.fahrenheit.toFixed(4)
+                }
+            } else if (type === 2) {
+                if (!isNaN(this.fahrenheit)) {
+                    this.celsius = (this.fahrenheit - 32) * (5/9)
+                    this.celsius = this.celsius.toFixed(4)
+                }
+            }
+        }
+    }
+}
+</script>
+
+<style scoped>
+.btn-primary {
+    color: #fff;
+    background-color: #6542f4;
+    border-color: #6542f4;
+}
+.counter-number {
+    font-size: 5rem;
+}
+</style>
